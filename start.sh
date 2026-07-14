de81305907b6041a9e68e4ae2efe4bfb168ebeb9
Mihir Vyas <mihirv126@gmail.com>
2026-07-14 17:34:36 +0530

    stable

--- start.sh
+++ start.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+echo "Starting FastAPI..."
 uvicorn main:app --host 0.0.0.0 --port 8000 &
+sleep 5
 
+echo "Checking port 8000..."
+curl http://127.0.0.1:8000/docs || echo "FastAPI not running"
+
+echo "Starting Streamlit..."
 streamlit run streamlit_app.py \
     --server.port=7860 \
     --server.address=0.0.0.0
